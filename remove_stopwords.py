c03beb69568de8ee0fea8ffa67b8c0357af8c751
bnymnsntrk <bnymnsntrk@gmail.com>
2019-12-24 23:35:13 +0300

    stopwords are deleted

--- remove_stopwords.py
+++ remove_stopwords.py
@@ -1,18 +1,33 @@
 import string
-
+import io
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import csv
 
 
 def to_text(user):
-    csv_file = '%s_tweets.csv' % (user)
-    txt_file = '%s_txt.txt' % (user)
+    csv_file = '%s_tweets.csv' % user
+    txt_file = '%s_txt.txt' % user
 
     with open(txt_file, "w") as my_output_file:
         with open(csv_file, "r") as my_input_file:
             [my_output_file.write(" ".join(row) + '\n') for row in csv.reader(my_input_file)]
         my_output_file.close()
+
+
+def remove_stops(user):
+    txt_in = '%s_txt.txt' % user
+    txt_out = '%s_filtered.txt' % user
+    stop_words = set(stopwords.words('english'))
+    file1 = open(txt_in)
+    line = file1.read()  # Use this to read file content as a stream:
+    words = line.split()
+    for r in words:
+        if not r in stop_words:
+            appendFile = open(txt_out, 'a')
+            appendFile.write("\n"+r)
+            appendFile.close()
+
 
 """
 def remove_punction_and_stopwords(msg):
@@ -24,3 +39,4 @@ def remove_punction_and_stopwords(msg):
 """
 
 to_text("bunthebig")
+remove_stops("bunthebig")
